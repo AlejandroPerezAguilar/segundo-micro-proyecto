79af28a6a1ddf79c449b276d0d103d5394b461cc
AlejandroPerezAguilar <alejandroperez@unicauca.edu.co>
2026-05-03 14:03:00 -0500

    Update fpga_top.vhd

--- fpga_top.vhd
+++ fpga_top.vhd
@@ -5,18 +5,18 @@ use work.package_memorias.ALL;
 
 entity fpga_top is
     port (
-        CLOCK_50 : in  std_logic;
-        KEY      : in  std_logic;
+        CLOCK_50 : in  std_logic; 	-- Reloj de 50MHz de la fpga
+        KEY      : in  std_logic;	-- Boton reset 
 
-        HEX0     : out std_logic_vector(6 downto 0);
-        HEX1     : out std_logic_vector(6 downto 0);
-        HEX2     : out std_logic_vector(6 downto 0)
+        HEX0     : out std_logic_vector(6 downto 0); 	-- Display unidades
+        HEX1     : out std_logic_vector(6 downto 0);	-- Display decenas
+        HEX2     : out std_logic_vector(6 downto 0)	-- Display centenas
     );
 end entity;
 
 architecture top of fpga_top is
 
-component divisorfreq
+component divisorfreq	-- Componente que divide la frecuencia del reloj 
 port(
 				clk :  in std_logic;
 				freq_sel      : in std_logic_vector(1 downto 0);
@@ -24,7 +24,7 @@ port(
 );
 	 end component;
     
-component micro2
+component micro2			-- Sistema que controla la ROM y la RAM 
 port (
             clk      : in  std_logic;
             rst      : in  std_logic;
@@ -36,69 +36,71 @@ port (
         );
     end component;	
 	 
-component bcd7seg
+component bcd7seg		-- Convierte un numero en BCD a  señales para display 7 segmentos 
 port(
 				bcd : in  std_logic_vector(3 downto 0);
             seg : out std_logic_vector(6 downto 0)
 );
 end component;
 
-signal clk_lento: std_logic;
-signal rst : std_logic;
+signal clk_lento: std_logic; 	-- Reloj lento generado por el divisor 
+signal rst : std_logic;			-- Señal de reset interna 
 
-signal data_out_s : std_logic_vector (DATA_WIDTH-1 downto 0);
+signal data_out_s : std_logic_vector (DATA_WIDTH-1 downto 0);		-- Dato que sale de micro2
 
 signal we_s : std_logic;
 signal addr_s : std_logic_vector (ADDR_WIDTH-1 downto 0);
 signal data_in_s :std_logic_vector (DATA_WIDTH-1 downto 0);
 
-signal valor : integer;
+signal valor : integer; 		-- Valor entero equivalente al dato de salida 
 signal c: integer;
 signal d: integer;
 signal u: integer;
 
+-- Centenas, decenas y unidades 
+
 begin
 
-rst <= NOT key;
+rst <= NOT key; 		-- Convierte el botón en reset activo en alto (porque el botón es activo en bajo)
 
 div : divisorfreq
 	port map (
-			clk      => CLOCK_50,
-			freq_sel => "10",
-			clk_out  => clk_lento
+			clk      => CLOCK_50,		-- Reloj original de la fpga 
+			freq_sel => "01",				-- Selecciona la velocidad del reloj
+			clk_out  => clk_lento		-- Reloj mas lento 
 );
 
 sistema : micro2
   port map (
-            clk      => clk_lento,
-            rst      => rst,
-            re       => '1',
-            we       => we_s,
+            clk      => clk_lento,	-- Usa el reloj lento
+            rst      => rst,			-- Señal de reset
+            re       => '1',			-- Lectura activa 
+            we       => we_s,			
             addr     => addr_s,
             data_in  => data_in_s,
-            data_out => data_out_s
+            data_out => data_out_s	-- Dato que se meustra en display 
         );
 
-valor <= to_integer(unsigned(data_out_s));
+valor <= to_integer(unsigned(data_out_s)); 	-- Convierte el dato binario a numero entero 
 		  
-c <= valor/100;
-d <= (valor mod 100) /10;
-u <= valor mod 10;
+c <= valor/100; 					-- Obtiene las centenas 
+d <= (valor mod 100) /10;		-- Obtiene las decenas  
+u <= valor mod 10;				-- Obtiene las unidades
 
-disp0 : bcd7seg
+disp0 : bcd7seg		-- Muestra las unidades
         port map (
-            bcd => std_logic_vector(to_unsigned(u, 4)),
+            bcd => std_logic_vector(to_unsigned(u, 4)),		-- Convierte el número a BCD de 4 bits
             seg => HEX0
         );
-disp1 : bcd7seg
+disp1 : bcd7seg		-- Muestra las decenas
         port map (
-            bcd => std_logic_vector(to_unsigned(d, 4)),
+            bcd => std_logic_vector(to_unsigned(d, 4)),		-- Convierte el número a BCD de 4 bits
             seg => HEX1
         );
 
-    disp2 : bcd7seg
+    disp2 : bcd7seg	-- Muestra las centenas 
         port map (
-            bcd => std_logic_vector(to_unsigned(c, 4)),
+            bcd => std_logic_vector(to_unsigned(c, 4)),		-- Convierte el número a BCD de 4 bits
             seg => HEX2
         );		  
 end architecture top;
