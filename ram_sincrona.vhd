c0d65c10afe1199559eaa3286961bcf9c848f9a4
AlejandroPerezAguilar <alejandroperez@unicauca.edu.co>
2026-05-02 21:56:12 -0500

    ,

--- ram_sincrona.vhd
+++ ram_sincrona.vhd
@@ -4,50 +4,53 @@ use ieee.numeric_std.all;
 
 entity ram_sincrona is
   generic (
-    DATA_WIDTH : positive := 8;
-    ADDR_WIDTH : positive := 4;
-    RDW_MODE   : string   := "READ_FIRST"  -- "READ_FIRST" | "WRITE_FIRST" | "NO_CHANGE"
+    DATA_WIDTH : positive := 8; 				-- Define el tamaño de cada dato de la RAm
+    ADDR_WIDTH : positive := 4;				-- Define el tamaño de la direccion de memoria
+    RDW_MODE   : string   := "READ_FIRST" -- "READ_FIRST" | "WRITE_FIRST" | "NO_CHANGE"
   );
   port (
-    clk      : in  std_logic;
-    rd_en    : in  std_logic := '1';
-    wr_en    : in  std_logic;
-    addr     : in  std_logic_vector(ADDR_WIDTH-1 downto 0);
-    data_in  : in  std_logic_vector(DATA_WIDTH-1 downto 0);
-    data_out : out std_logic_vector(DATA_WIDTH-1 downto 0)
+    clk      : in  std_logic; 				-- Reloj del sistema 
+    rd_en    : in  std_logic := '1'; 		-- Habilita la lectura de la RAM
+    wr_en    : in  std_logic;					-- Habilita la escritura de la RAM
+    addr     : in  std_logic_vector(ADDR_WIDTH-1 downto 0); -- Direccion de memoria a usar 
+    data_in  : in  std_logic_vector(DATA_WIDTH-1 downto 0); -- Dato que se va a escribir en la RAM
+    data_out : out std_logic_vector(DATA_WIDTH-1 downto 0)  -- Dato leido desde la RAM
   );
 end entity;
 
 architecture rtl of ram_sincrona is
-  type ram_type is array (0 to 2**ADDR_WIDTH-1) of std_logic_vector(DATA_WIDTH-1 downto 0);
-  signal mem      : ram_type;
-  signal q_reg    : std_logic_vector(DATA_WIDTH-1 downto 0);
-  signal addr_i   : integer range 0 to 2**ADDR_WIDTH-1;
+  type ram_type is array (0 to 2**ADDR_WIDTH-1) of std_logic_vector(DATA_WIDTH-1 downto 0); -- Define el tamaño de la memoria y el tamaño de cada dato
+  signal mem      : ram_type; -- Señal que representa la RAM
+  signal q_reg    : std_logic_vector(DATA_WIDTH-1 downto 0); -- Señal donde se guarda el dato leido
+  signal addr_i   : integer range 0 to 2**ADDR_WIDTH-1; -- Señal entera que representa la direccion de la memoria 
 
   attribute ramstyle : string;
-  attribute ramstyle of mem : signal is "M9K";
+  attribute ramstyle of mem : signal is "M9K"; -- Hace que la ROM se implemente en la memoria interna del FPGA
 begin
-  addr_i <= to_integer(unsigned(addr));
+  addr_i <= to_integer(unsigned(addr)); -- Convierte la direccion de binario a entero 
 
   process(clk)
   begin
     if rising_edge(clk) then
-      if wr_en = '1' then
-        mem(addr_i) <= data_in;
-        if RDW_MODE = "WRITE_FIRST" then
-          q_reg <= data_in;
-        elsif RDW_MODE = "READ_FIRST" then
-          q_reg <= mem(addr_i);
+      if wr_en = '1' then 			-- Si la escritura esta habilitada 
+        mem(addr_i) <= data_in; 	-- Guarda data_in en la posición indicada por addr_i 
+
+        if RDW_MODE = "WRITE_FIRST" then -- Si se escribe y se quiere mostrar el dato nuevo
+          q_reg <= data_in; 		-- La salida toma el dato que se esta escribiendo 
+			 
+        elsif RDW_MODE = "READ_FIRST" then -- Si se escribe y se quiere mostrar el dato anterior
+          q_reg <= mem(addr_i); -- La salida muestra el dato que estaba antes en esa dirección
+
         else
           null; -- NO_CHANGE
         end if;
       else
-        if rd_en = '1' then
-          q_reg <= mem(addr_i);
+        if rd_en = '1' then 		-- Si no se escribe y la lectura esta habilitada
+          q_reg <= mem(addr_i);  -- Lee la posicion indicada y guarda el dato en q_reg
         end if;
       end if;
     end if;
   end process;
 
-  data_out <= q_reg;
+  data_out <= q_reg; -- Envía el dato leído a la salida
 end architecture;
--- rom_sync.vhd
+++ rom_sync.vhd
@@ -4,37 +4,41 @@ use ieee.numeric_std.all;
 
 entity rom_sync is
   generic (
-    DATA_WIDTH : positive := 8;
-    ADDR_WIDTH : positive := 4
+    DATA_WIDTH : positive := 8; 		-- Numero de bits de cada dato almacenado
+    ADDR_WIDTH : positive := 4 		-- Numero de bits que define el tamaño de la memoria 
   );
   port (
-    clk      : in  std_logic;
-    addr     : in  std_logic_vector(ADDR_WIDTH-1 downto 0);
-    data_out : out std_logic_vector(DATA_WIDTH-1 downto 0)
+    clk      : in  std_logic; 		-- Reloj del sistema 
+    addr     : in  std_logic_vector(ADDR_WIDTH-1 downto 0); -- Direccion de memoria a leer
+    data_out : out std_logic_vector(DATA_WIDTH-1 downto 0) -- Dato leido de la ROM
   );
 end entity;
 
 architecture rtl of rom_sync is
-  type rom_type is array (0 to 2**ADDR_WIDTH-1) of std_logic_vector(DATA_WIDTH-1 downto 0);
+  type rom_type is array (0 to 2**ADDR_WIDTH-1) of std_logic_vector(DATA_WIDTH-1 downto 0); -- Define el tamaño de la memoria y el tamaño de cada dato
   constant mem : rom_type := (
-    0 => x"AA", 1 => x"55", 2 => x"F0", 3 => x"0F",
-    4 => x"FF", 5 => x"00",
-    others => (others => '0')
+    0 => x"AA" 		--	170 decimal
+	 , 1 => x"55" 		--	85 decimal
+	 , 2 => x"F0", 	--	240 decimal
+	 3 => x"0F", 		--	15 decimal
+    4 => x"FF",		--	255 decimal 
+	 5 => x"00",		--	0 decimal 
+    others => (others => '0') 	--	las posiciones no definidas se hacen 0
   );
-  signal q_reg : std_logic_vector(DATA_WIDTH-1 downto 0);
-  signal addr_i: integer range 0 to 2**ADDR_WIDTH-1;
+  signal q_reg : std_logic_vector(DATA_WIDTH-1 downto 0); 	-- La señal guarda temporalmente el dato leido de la ROM
+  signal addr_i: integer range 0 to 2**ADDR_WIDTH-1; 			-- Señal entera que representa la dirección de la memoria 
 
   attribute romstyle : string;
-  attribute romstyle of mem : constant is "M9K";
+  attribute romstyle of mem : constant is "M9K"; -- Hace que la ROM se implemente en la memoria interna del FPGA
 begin
-  addr_i <= to_integer(unsigned(addr));
+  addr_i <= to_integer(unsigned(addr)); 	-- Convierte la direccion de binario a entero para acceder a la ROM
 
   process(clk)
   begin
     if rising_edge(clk) then
-      q_reg <= mem(addr_i);
+      q_reg <= mem(addr_i); 	-- Lee el dato de la ROM en la dirección indicada y lo guarda en el registro
     end if;
   end process;
 
-  data_out <= q_reg;
+  data_out <= q_reg;		--	Asigna el daato leido a la salida
 end architecture;
